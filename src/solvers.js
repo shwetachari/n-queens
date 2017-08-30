16a8b0f45891ce8508e435063e08eba850dfa809
shwetachari <shwetachari@gmail.com>
2017-08-30 16:15:46 -0700

    rooks complete

--- src/solvers.js
+++ src/solvers.js
@@ -13,18 +13,54 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
+};
 
+/*
+[
+  [1, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 1, 0],
+  [0, 0, 0, 1]
+]
+*/
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = makeEmptyMatrix(n);
+  
+  var placeRooks = function(row, column, rooksLeft) {
+    //  base case if rooksLeft === 0
+    if (rooksLeft === 0) {
+      return;
+      //  return
+    } else {
+      solution[row][column] += 1;
+      //  call placeRooks(row + 1, column + 1, rooksLeft - 1)
+      placeRooks(row + 1, column + 1, rooksLeft - 1);
+    }
+    
+  
+  };
+  placeRooks(0, 0, n);
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  for (var i = 1; i <= n; i++) {
+    if (i === 1) {
+      solutionCount = 1;
+    }
+    solutionCount = solutionCount * i;
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -41,6 +77,16 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
+  // pick a start point (not the corners)
+  // iterate over all possible starting points
+    // iterate over all possible points in next column
+    // check for conflicts, place
+      //repeat until end of board
+    //last column: if can't find non-conflicting spot
+      //solution does not work
+      //move onto next solution
+    //last column: if we can find a working solution
+      //add 1
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
